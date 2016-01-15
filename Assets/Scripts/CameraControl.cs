c5797df0a130cffa0dcc2961a140984ecf797274
Jannizzle <jannis.xenakis@gmx.de>
2016-01-15 16:00:14 +0100

    Update 1

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -10,6 +10,7 @@ public class CameraControl : MonoBehaviour
 	float origZ;
 	public Transform goal;
 	Vector3 diff;
+	Camera camera;
 
 
 	// Use this for initialization
@@ -17,6 +18,7 @@ public class CameraControl : MonoBehaviour
 	{
 		player = GameObject.FindGameObjectWithTag ("Player");
 		origZ = transform.position.z;
+		camera = gameObject.GetComponent<Camera> ();
 	}
 	
 	// Update is called once per frame
@@ -30,15 +32,31 @@ public class CameraControl : MonoBehaviour
 			transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, transform.position.z);
 			//transform.Translate (new Vector3 (Mathf.Abs (goal.position.x - transform.position.x), Mathf.Abs (goal.position.y - transform.position.y), transform.position.z));
 		}
+		if (Input.GetKeyDown ("p")) {
+			adjustCamera (0);
+		}
+		if (Input.GetKeyDown ("o")) {
+			adjustCamera (1);
+		}
 	
 	}
 
 	void panToGoal ()
 	{
 		goal = GameObject.FindGameObjectWithTag ("Goal").transform;
-		diff = new Vector3 (Mathf.Abs (goal.position.x - transform.position.x), Mathf.Abs (goal.position.y - transform.position.y), origZ);
+		diff = new Vector3 (goal.position.x - transform.position.x, goal.position.y - transform.position.y, origZ);
 		transform.Translate (diff * Time.deltaTime * 2.0f);
 		Debug.Log (diff);
 
+	}
+
+	void adjustCamera (int mode)
+	{
+		if (mode == 0) {
+			camera.orthographicSize = camera.orthographicSize + 5;
+		}
+		if (mode == 1) {
+			camera.orthographicSize = camera.orthographicSize - 5;
+		}
 	}
 }
